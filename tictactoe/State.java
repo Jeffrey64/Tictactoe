1635cfd6ac4f31d3d600eb3686eff20ca8253c52
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2024-01-11 11:44:02 -0800

    user interface

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -7,8 +7,17 @@ public class State
 {
     private int gameState = Constants.STANDBY;
     private int whoseMove = Constants.X;
+        return this.whoseMove
     private String xName = "";
     private String oName = "";
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+
+    public boolean isWinner() {
+      // You will write this code!!
+    }
+
+    public boolean isTie() {
+      // You will write this code too!!
+    }
 }
 
