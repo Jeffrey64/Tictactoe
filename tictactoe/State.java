a2ef245e31d817a4bb80849aee09a848949e88b8
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2024-01-12 11:44:21 -0800

    started on coding segment

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -7,17 +7,43 @@ public class State
 {
     private int gameState = Constants.STANDBY;
     private int whoseMove = Constants.X;
-        return this.whoseMove
-    private String xName = "";
-    private String oName = "";
-    private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
 
+    public void setwhoseMove(int whoseMove) {
+        this.whoseMove = whoseMove;
+    }
+
+    public int getwhoseMove() {
+        return this.whoseMove;
+    }
+    private String xName = "";
+    public int getxName () {
+        return this.xName;
+    }
+
+    public void setxName(int xName) {
+        this.xName = xName
+    }
+    private String oName = "";
+    public int getoName() {
+        return this.oName;
+    }
+
+    public void setoName(int oName) {
+        this.oName = oName;
+    }
+    private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+        public int getBoardCell(int row, int col) {
+            return this.board[row][col];
+        }
+        public void setBoardCell(int row, int col, int value) {
+            this.board[row][col] = value;
+        }
     public boolean isWinner() {
-      // You will write this code!!
+        // You will write this code!!
     }
 
     public boolean isTie() {
-      // You will write this code too!!
+        // You will write this code too!!
     }
 }
 
