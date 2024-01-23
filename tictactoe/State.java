3ee59f298cc145b4ed81ade5e0315b815468ad56
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2024-01-23 11:44:58 -0800

    user interface

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -6,38 +6,54 @@ package tictactoe;
 public class State
 {
     private int gameState = Constants.STANDBY;
+
+    public int getGameState() {
+        return this.gameState;
+    }
+    
+    public void setGameState (int gameState) {
+        this.gameState = gameState;
+    }
+
     private int whoseMove = Constants.X;
 
-    public void setwhoseMove(int whoseMove) {
+    public int getWhoseMove() {
+        return this.whoseMove;
+    }
+    
+    public void setWhoseMove(int whoseMove) {
         this.whoseMove = whoseMove;
     }
 
-    public int getwhoseMove() {
-        return this.whoseMove;
-    }
     private String xName = "";
-    public int getxName () {
+
+    public String getxName() {
         return this.xName;
     }
 
-    public void setxName(int xName) {
-        this.xName = xName
-    }
-    private String oName = "";
-    public int getoName() {
-        return this.oName;
+    public void setxName(String xName) {
+        this.xName = xName;
     }
 
-    public void setoName(int oName) {
+    private String oName = "";
+
+    public String oName() {
+        return this.oName;
+    }
+    
+    public void setoName(String oName) {
         this.oName = oName;
     }
+
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
-        public int getBoardCell(int row, int col) {
-            return this.board[row][col];
-        }
-        public void setBoardCell(int row, int col, int value) {
-            this.board[row][col] = value;
-        }
+    
+    public int getBoardCell(int row, int col) {
+        return this.board[row][col];
+    }
+    public void setBoardCell(int row, int col, int value) {
+        this.board[row][col] = value;
+    }
+    
     public boolean isWinner() {
         // You will write this code!!
     }
